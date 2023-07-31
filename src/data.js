b9670a333056d02575e9c73d510219efc58f582b
Chinmayee Ahire <theroadtotae@gmail.com>
2023-07-31 21:25:10 +0530

    Added navbar, announcement ans slider components

---
+++ src/data.js
@@ -0,0 +1,30 @@
+
+import slider from "./images/slider.png"
+import slider1 from "./images/slider1-r.png"
+import slider2 from "./images/slider2-re.png"
+
+export const sliderItems=[
+    {
+        id:1,
+        img: slider,
+        title: "SUMMER SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "f5fafd",
+    },
+    {
+        id:2,
+        img: slider1,
+        title: "WINTER SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "fcf1ed",
+    },
+    {
+        id:3,
+        img: slider2,
+        title: "POPULAR SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "FBF0F4",
+    }
+
+]
+
