e7081b28bddd9470404dcfa7a32c7dda6eb1d474
Chinmayee Ahire <theroadtotae@gmail.com>
2023-08-01 16:13:55 +0530

    Added Category, Product, Products components

--- src/data.js
+++ src/data.js
@@ -2,6 +2,17 @@
 import slider from "./images/slider.png"
 import slider1 from "./images/slider1-r.png"
 import slider2 from "./images/slider2-re.png"
+import shirt from "./images/shirt.webp";
+import hoodie from "./images/winter.jpg";
+import popular from "./images/jackets.webp";
+import prod1 from "./images/prod1.webp";
+import prod2 from "./images/prod2.webp";
+import prod3 from "./images/prod3.jpg";
+import prod4 from "./images/prod4.webp";
+import prod5 from "./images/prod5.webp";
+import prod6 from "./images/prod6.webp";
+import prod7 from "./images/prod7.jpg";
+import prod8 from "./images/prod8.webp";
 
 export const sliderItems=[
     {
@@ -28,3 +39,54 @@ export const sliderItems=[
 
 ]
 
+export const categories=[
+    {
+        id: 1,
+        img: shirt,
+        title: "SHIRT STYLE!",
+    },
+    {
+        id: 2,
+        img: hoodie,
+        title: "HOODIE LOVE", 
+    },
+    {
+        id: 3,
+        img: popular,
+        title: "SUMMER ESSENTIALS",
+    }
+]
+
+export const popularProducts=[{
+    id: 1,
+    img: prod1
+},
+{
+   id:2,
+   img: prod2
+},
+{
+    id:3,
+   img: prod3
+},
+{
+    id:4,
+   img: prod4
+},
+{
+    id:5,
+    img: prod5
+},
+{
+    id:6,
+   img: prod6
+},
+{
+    id:7,
+    img: prod7
+},
+{
+    id:8,
+    img: prod8
+}
+]
